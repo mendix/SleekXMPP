69035a31abff4ebd493312a0a1e6a3b80f14c9b1
Achiel van der Mandele <achiel@elsuxor.net>
2011-02-03 10:44:51 +0100

    removed unused methods inherited from xep_0047 and threading stuff

--- sleekxmpp/plugins/xep_0065.py
+++ sleekxmpp/plugins/xep_0065.py
@@ -4,7 +4,6 @@ license to be announced, some type of opensource though
 """
 
 from __future__ import division, with_statement, unicode_literals
-import sys
 import struct
 import logging
 import threading
@@ -37,38 +36,18 @@ def sendCloseStream(xmpp, to, sid):
 class xep_0065(xep_0096.FileTransferProtocol):
     XMLNS = 'http://jabber.org/protocol/bytestreams'
     SOCKET_NEGOTIATE_XPATH = MatchXPath('{%s}iq/{%s}query' % (DEFAULT_NS, XMLNS))
-    OPEN_STREAM_XPATH = MatchXPath('{%s}iq/{%s}open'  %(DEFAULT_NS, XMLNS))
-    CLOSE_STREAM_XPATH = MatchXPath('{%s}iq/{%s}close' %(DEFAULT_NS, XMLNS))
-
        
     def plugin_init(self):
         self.xep = '0065'
         self.description = 'Socks5 Bytestreams'
         self.incoming_files = {}
 
-        self.acceptTransfers = self.config.get('acceptTransfers', True)
-        self.saveDirectory = self.config.get('saveDirectory', '/tmp/')
-        self.maxSessions = self.config.get('maxSessions', 2)
-        self.transferTimeout = self.config.get('transferTimeout', 120) #how long we should wait between data messages until we consider the stream invalid
-        #thread setup
-        self.streamSessions = {} #id:thread
-        self.__streamSetupLock = threading.Lock()
-        #Register the xmpp stanzas used in this plugin
         register_stanza_plugin(Iq, StreamHosts)
         register_stanza_plugin(Iq, StreamHostUsed)
 
-        # create callbacks:
         socket_negotiation_callback = Callback('negotiate_file_transfer_socket', xep_0065.SOCKET_NEGOTIATE_XPATH, self._handle_socket_negotiation)
-        open_stream_callback = Callback('xep_0065_open_stream', xep_0065.OPEN_STREAM_XPATH, self._handle_incoming_transfer_request, thread=True)
-        close_stream_callback = Callback('xep_0065_close_stream', xep_0065.CLOSE_STREAM_XPATH, self._handle_stream_closed, thread=False)
 
-        #add handlers to listen for incoming requests:
-        self.xmpp.register_handler(close_stream_callback)
-        self.xmpp.register_handler(open_stream_callback)
         self.xmpp.register_handler(socket_negotiation_callback)
-
-        #Event handler to allow session threads to call back to the main processor to remove the thread
-        self.xmpp.add_event_handler(STREAM_CLOSED_EVENT, self._event_close_stream, threaded=True, disposable=False)
         
     def post_init(self):
         xep_0096.FileTransferProtocol.post_init(self)
@@ -76,77 +55,21 @@ class xep_0065(xep_0096.FileTransferProtocol):
             self.xmpp.plugin['xep_0030'].add_feature(xep_0065.XMLNS)
         
     def sendFile(self, fileName, to, threaded=True, sid=None):
-        '''
-        Sends a file to the intended receiver if the receiver is available and 
-        willing to accept the transfer.  If the send is requested to be threaded 
-        the session sid will be returned, otherwise the method will block until 
-        the file has been sent and the session closed.
-        
-        The returned sid can be used to check on the status of the transfer or 
-        cancel the transfer.
-        
-        Error Conditions:
-        -IOError will be raised if the file to be sent is not found
-        -TooManySessionsException will be raised if there are already more than 
-        self.maxSessions running (configurable via plugin configuration)
-        -Exception will be raised if the sender is not available
-        -NotAcceptableException will be raised if the sender denies the transfer request
-        or if the sender full JID is equal to the recipient 
-        -InBandFailedException will be raised if there is an error during the
-        file transfer
-        '''
         pass
     
     def getSessionStatus(self, sid):
-        '''
-        Returns the status of the transfer specified by the sid.  If the session
-        is not found none will be returned.
-        '''
-        session = self.streamSessions.get(sid)
-        if session:
-            return session.getStatus()
-        else:
-            return None
+        None
         
     def getSessionStatusAll(self):
-        dict = {}
-        for session in self.streamSessions.values():
-            dict[session.sid] = session.getStatus()
-        
-        return dict
+        return {}
         
     def cancelSend(self, sid):
-        '''
-        cancels an outgoing file transfer.
-        If the session is not found, method will pass
-        '''
-        session = self.streamSessions.get(sid)
-        if session:
-            session.cancelStream()
+        pass
             
-    def setAcceptStatus(self, status):
-        '''
-        sets if plugin will accept in-band file transfers or not.
-        if switching from true to false any currently working sessions will 
-        finish
-        '''
-        self.acceptTransfers = status
-
     def start_receive_file(self, file_name, file_length, sid):
       self.incoming_files[sid] = {"name" : file_name, "length" : file_length}
       log.debug("A file was announced to me: %s " % file_name)
 
-    def _handle_incoming_transfer_request(self, iq):
-      print("handling incoming request")
-        
-
-    def _handle_stream_closed(self, iq):
-      print("handle stream closed")
-        
-
-    def _event_close_stream(self, iq):
-      print("event close stream")
-        
     def _handle_socket_negotiation(self, iq):
       sid = iq['streamhosts']['sid']
       host = None
@@ -159,7 +82,6 @@ class xep_0065(xep_0096.FileTransferProtocol):
       byte_stream_session = ByteStreamSession(self.xmpp, sid, requester_jid, target_jid, file_size)
 
       for (jid_key, s) in iq['streamhosts']['hosts'].iteritems():
-        print("Available streamhost: %s:%s" % (s['host'], s['port']))
         host = s['host']
         port = s['port']
         jid = jid_key
@@ -320,50 +242,3 @@ class StreamHostUsed(ElementBase):
   plugin_attrib = 'streamhost-used'
   name = 'streamhost-used'
   interfaces = set(('jid',))
-
-'''
-Override of the threading.Event class to make the implementation work like 
-python 2.7
-'''
-def Event(*args, **kwargs):
-    if sys.version_info < (2,7):
-        return _Event(*args, **kwargs)
-    else:
-        return threading.Event(*args, **kwargs)
-
-class _Event(object):
-
-    #Modification of Event class from python 2.6 because the 2.7 version is better
-
-    def __init__(self):
-        self.__cond = threading.Condition(threading.Lock())
-        self.__flag = False
-
-    def isSet(self):
-        return self.__flag
-
-    is_set = isSet
-
-    def set(self):
-        self.__cond.acquire()
-        try:
-            self.__flag = True
-            self.__cond.notify_all()
-        finally:
-            self.__cond.release()
-
-    def clear(self):
-        self.__cond.acquire()
-        try:
-            self.__flag = False
-        finally:
-            self.__cond.release()
-
-    def wait(self, timeout=None):
-        self.__cond.acquire()
-        try:
-            if not self.__flag:
-                self.__cond.wait(timeout)
-            return self.__flag
-        finally:
-            self.__cond.release()
